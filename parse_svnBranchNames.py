7dc36bea3e64d40d7118380daf68acd0d7328afd
Nathan Dumar <njdumar@gmail.com>
2013-07-30 09:22:06 -0500

    Updated to work a lot better, soon I'll finish this script, maybe

--- parse_svnBranchNames.py
+++ parse_svnBranchNames.py
@@ -2,16 +2,13 @@
 
 import re, sys, getopt
 import os
-from subprocess import call
+import subprocess
 
 # Take a list of svn repository directories:
-# svn list --depth infinity <full url> | grep -v '/library/\|/extern/\|/tools/\|/design/\|rel_branches\|trunk/\|tags/\|repo_lock\|\.' > smr6_list.txt
+# svn list --depth infinity <full url> | grep -v '/library/\|/extern/\|/tools/\|/design/\|rel_branches\|trunk/\|tags/\|repo_lock\|\.' > list.txt
 # and parse them. Find the root folder containing actual
-# branches (not folders containing folders that have branches) and create a gitconfig compatible output.
+# branches (not folders containing folders that have branches) and create a .git/config compatible output.
 # This will let me choose exactly which branches I want in my git svn clone, instead of waiting for everything
-
-# Obviously, create the list.txt from the command above. I really should have python do it, but that command
-# will take a very long time to run on a complex repo (lots of directories/branches)
 
 def main(argv):
 
@@ -22,15 +19,21 @@ def main(argv):
     generate = False
 
     try:
-        opts, args = getopt.getopt(argv,"hi:o:s:u:g:",["ifile=","ofile=","svn=","url=","generate="])
+        opts, args = getopt.getopt(argv,"hi:o:s:u:g",["ifile=","ofile=","svn=","url=","generate="])
     except getopt.GetoptError:
         print 'test.py -i <inputfile> -o <outputfile> -s <svn name>'
         sys.exit(2)
 
     for opt, arg in opts:
         if opt == '-h':
-            print 'test.py -i <inputfile> -o <outputfile>  -s <svn name>'
-            sys.exit()
+            print 'parse_svnBranches.py -i <inputfile> -o <outputfile>  -s <svn name> -u <url>'
+            print '---------------------------------------------------------------------------'
+            print '--ifile    -i -> file that contains a list of svn branches from the \'svn list\' command'
+            print '--ofile    -o -> .gitconfig output file'
+            print '--svn      -s -> Name of the svn module (repo)'
+            print '--url      -u -> url of the svn repo (minus the repo name/module)'
+            print '--generate -g -> If specified, create the input file first'
+            exit(0)
         elif opt in ("-i", "--ifile"):
             inputfile = arg
         elif opt in ("-o", "--ofile"):
@@ -42,79 +45,92 @@ def main(argv):
         elif opt in ("-g", "--generate"):
             generate = arg
 
-    #print 'Input file is "', inputfile
-    #print 'Output file is "', outputfile
-    #call(["svn", "list", "--depth", "infinity", ])
-    if generate:
+    # create the output
+    out = open(outputfile, 'w')
 
-        for svnRepo in svn:
-
-            os.system("svn list --depth infinity " + url + "/" + svnRepo + " | grep -v '/library/\|/extern/\|/tools/\|/design/\|rel_branches\|trunk/\|tags/\|repo_lock\|\.' > " + inputfile)
-            print "svn list --depth infinity " + url + "/" + svnRepo + " | grep -v '/library/\|/extern/\|/tools/\|/design/\|rel_branches\|trunk/\|tags/\|repo_lock\|\.' > " + inputfile
-
-        exit(0)
-
-    f = open(inputfile)
-    inputFile = f.readlines()
-    f.close
-
-    extra = list()
-    branchDir = list()
-    branchNames = list()
-    base = ''
-    entry = ''
-    first = True
-
-    for index, line in enumerate(inputFile):
-        copy = False 
-        branch = re.search(r'(.*/)(.*)/$', line)
-
-        regex = r".*" + line[:-1] + r".*/.*"
-
-        if index < len(inputFile) - 1:
-
-            if re.search(regex, inputFile[index+1]):
-
-                # This dir contatins branches, it is not a branch itself
-                extra.append(line)
-
-            elif base != branch.group(1):
-
-                if first == False:
-                    if base in branchDir:
-                        branchNames[branchDir.index(base)] += ("," + entry)
-                    else:
-                        branchDir.append(base)
-                        branchNames.append(entry)
-
-                base = branch.group(1)
-                entry = branch.group(2)
-
-                first = False
-
-            else:
-                entry = entry + "," + branch.group(2)
-
-    # Add the last entry
-    branchDir.append(base)
-    branchNames.append(entry)
+    # Create the begining of the config file
+    out.write("[core]\n")
+    out.write("    repositoryformatversion = 0\n")
+    out.write("    filemode = false\n")
+    out.write("    bare = false\n")
+    out.write("    logallrefupdates = true\n")
+    out.write("    symlinks = false\n")
+    out.write("    ignorecase = true\n")
 
     for svnRepo in svn:
-        print "[core]"
-        print "    repositoryformatversion = 0"
-        print "    filemode = false "
-        print "    bare = false "
-        print "    logallrefupdates = true"
-        print "    symlinks = false"
-        print "    ignorecase = true"
-        print "[svn-remote \"svn\"]"
-        print "    url = " + url +"/" + svnRepo
-        print "    fetch = trunk:refs/remotes/" + svnRepo + "/trunk"
-        print "    tags = tags/*:refs/remotes/" + svnRepo + "/tags/*"
-        print "    branches = rel_branches/*:refs/remotes/" + svnRepo + "/rel_branches/*"
 
-    for index, line in enumerate(branchDir):
-        print "#    branches = " + line + "{" + branchNames[index] + "}:refs/remotes/" + svn + "/" + line + "*"
+        if generate:
+
+            svnList = "svn list --depth infinity " + url + "/" + svnRepo + " | grep -v '/library/\|/extern/\|/tools/\|/design/\|rel_branches\|trunk/\|tags/\|repo_lock\|(\|)\|\.'"
+            output = subprocess.check_output(svnList, shell=True)
+
+            # The reason this is even put into a file in the first place is so that I can analyze the results before the git config is created
+            # There were some issues getting the 'svn list' just right
+            f = open(inputfile, 'w')
+            f.write(output)
+            f.close
+
+        f = open(inputfile)
+        inputFile = f.readlines()
+        f.close
+
+        extra = list()
+        branchDir = list()
+        branchNames = list()
+        base = ''
+        entry = ''
+        first = True
+
+        # Search for every real branch, not folders containing branches, and not folders in branches. Just the branches.
+        for index, line in enumerate(inputFile):
+            copy = False 
+
+            # Get the directory of the branch, group(1) and the branch name, group(2)
+            branch = re.search(r'(.*/)(.*)/$', line)
+
+            regex = r".*" + line[:-1] + r".*/.*"
+
+            if index < len(inputFile) - 1:
+
+                if re.search(regex, inputFile[index+1]):
+
+                    # This dir contatins branches, it is not a branch itself. This is saved just so I can look at it if I wanted too
+                    extra.append(line)
+
+                elif base != branch.group(1):
+
+                    if first == False:
+
+                        # Either create a new list of branch names, or add to a list that already exists, but was out of order (there were
+                        # more branches found in that directory later on in the branch list, which is NOT sorted)
+                        if base in branchDir:
+                            branchNames[branchDir.index(base)] += ("," + entry)
+                        else:
+                            branchDir.append(base)
+                            branchNames.append(entry)
+
+                    base = branch.group(1)
+                    entry = branch.group(2)
+
+                    first = False
+
+                else:
+                    entry = entry + "," + branch.group(2)
+
+        # Add the last entry (should be the last branch)
+        branchDir.append(base)
+        branchNames.append(entry)
+
+        out.write("[svn-remote \"svn\"]\n")
+        out.write("    url = " + url +"/" + svnRepo + "\n")
+        out.write("    fetch = trunk:refs/remotes/" + svnRepo + "/trunk\n")
+        out.write("    tags = tags/*:refs/remotes/" + svnRepo + "/tags/*\n")
+        out.write("    branches = rel_branches/*:refs/remotes/" + svnRepo + "/rel_branches/*\n")
+
+        for index, line in enumerate(branchDir):
+            out.write("    branches = " + line + "{" + branchNames[index] + "}:refs/remotes/" + svnRepo + "/" + line + "*\n")
+
+        out.close
 
 if __name__ == "__main__":
     main(sys.argv[1:])
