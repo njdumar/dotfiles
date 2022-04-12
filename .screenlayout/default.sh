33e76afbd3c3b0363c3718d18cb2722718b0a20f
Nathan Dumar <njdumar@gmail.com>
2022-04-12 07:56:04 -0500

    config updates

--- .screenlayout/default.sh
+++ .screenlayout/default.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-I-0 --off --output DVI-I-1 --off --output DP-0 --off --output DP-1 --off --output DP-2 --primary --mode 3440x1440 --pos 0x815 --rotate normal --output DP-3 --mode 3840x2160 --pos 3440x0 --rotate left
+xrandr --output DVI-I-0 --off --output DVI-I-1 --off --output DP-0 --off --output DP-1 --off --output DP-2 --primary --mode 3440x1440 --pos 0x244 --rotate normal --output DP-3 --mode 2560x1440 --pos 3440x0 --rotate right
